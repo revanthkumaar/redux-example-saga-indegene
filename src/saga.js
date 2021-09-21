6665f45bf77a4c7865ee81e9196f41be577f45f9
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-21 11:07:00 +0530

    laksjdlkjlkjalscj
    alskjd

--- src/saga.js
+++ src/saga.js
@@ -1,3 +1,31 @@
 import {takeLatest, call, put} from 'redux-saga/effects';
 import axios from "axios";
 
+//this saga/generator function watches for the trigger of the action mentioned 
+export function* watcherSaga(){
+    yield takeLatest("API_CALL_REQUEST", workerSaga);
+}
+
+//api call
+function fetchDog(){
+    return axios({
+        method: "get",
+        url: "https://dog.ceo/api/breeds/image/random"
+    });
+}
+
+
+//once the above trigger happens this below gen function gets triggered
+function* workerSaga(){
+    try{
+        const response = yield(call(fetchDog))
+        const dog = response.data.message;
+
+        //dispatch an event that says we got a success response
+
+        yield put({type: "API_CALL_SUCCESS", dog})
+    }
+
+    catch(error){yield put({type: "API_CALL_FAILURE", error})}
+    
+}
