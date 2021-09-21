8c362b31db11b7b9e6ede36d930b742b1f895a13
revanthkumaar <iamrevanthkumar@gmail.com>
2021-09-21 10:33:06 +0530

    asda
    ada

--- src/index.js
+++ src/index.js
@@ -4,14 +4,23 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import { reducer } from "./redux";
+import logger from 'redux-logger'
+import createSagaMiddleware from 'redux-saga';
+
+//intitialize the saga middleware
+const sagaMiddleware = createSagaMiddleware()
+
+//store of redux
+let store = createStore(reducer, applyMiddleware(logger, sagaMiddleware));
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}> <App /> </Provider>,
+   
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
 reportWebVitals();
---
+++ src/saga.js
@@ -0,0 +1,3 @@
+import {takeLatest, call, put} from 'redux-saga/effects';
+import axios from "axios";
+
